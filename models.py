4a7623e15d645098b026de21100441a24fea4c51
Ren <renato_s_silva@hotmail.com>
2019-04-20 15:16:19 +1000

    preparing for the class

--- models.py
+++ models.py
@@ -14,49 +14,38 @@ class Net(nn.Module):
         super(Net, self).__init__()
         
         ## TODO: Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 1. This network takes in a square (same width and height), 3-colour image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(3, 32, 5)    #  224 x 224
+        
+        self.conv1 = nn.Conv2d(3, 32, 5)    #  224 x 224 # this time we left the colour channels in 
         self.conv1_bn = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, 3)  
         self.conv2_bn = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 128, 3)
         self.conv3_bn = nn.BatchNorm2d(128)
-        #self.conv4 = nn.Conv2d(128, 256, 3)  
-        #self.conv4_bn = nn.BatchNorm2d(256)
         
         self.maxPool = nn.MaxPool2d(2, 2)         
-        # self.fc1 = nn.Linear(256 * 12 * 12 , 1000)
-        # self.fc2 = nn.Linear(1000 , 1)
 
-        self.fc3 = nn.Linear(128 * 26 * 26 , 10)
-        self.fc4 = nn.Linear(10 , 1)
+        self.fc1 = nn.Linear(128 * 26 * 26 , 10)
+        self.fc2 = nn.Linear(10 , 1)
         
+        # sigmoid returns a value between 0 -> 1
         self.sigmoid = nn.Sigmoid()
         
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
+
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
 
         x = self.maxPool(F.relu(self.conv1_bn(self.conv1(x)))) # 220 x 220 / 2 = 110 x 110
         x = self.maxPool(F.relu(self.conv2_bn(self.conv2(x)))) # 108 x 108 / 2 = 54 x 54
         x = self.maxPool(F.relu(self.conv3_bn(self.conv3(x)))) # 52  x 52  / 2 = 26 x 26
-        # x = self.maxPool(F.relu(self.conv4_bn(self.conv4(x)))) # 24  x 24  / 2 = 12 x 12
-        
-        # x = F.relu(self.fc1(x.view(x.size(0), -1)))
-        # x = self.sigmoid(self.fc2(x))
+  
+        x = F.relu(self.fc1(x.view(x.size(0), -1)))
+        x = self.sigmoid(self.fc2(x))
 
-        x = F.relu(self.fc3(x.view(x.size(0), -1)))
-        x = self.sigmoid(self.fc4(x))
-
-        
-        # a modified x, having gone through all the layers of your model, should be returned
         return x
 
